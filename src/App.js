d629dca83b6861cb930bd69e724738c81fe853de
Urozboev <mirjalol0406@gmail.com>
2022-12-11 22:02:02 +0500

    vercel example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'antd';
+
 function App() {
   return (
     <div>
-      Hello World
+      Hello
+      <h6>Vercelga deploy qilindi</h6>
+      <Button type='primary'>Click</Button>
+      <Navbar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'antd';
+import { Container } from 'react-bootstrap';
+
+function Navbar() {
+    return (
+        <Container>
+            <Button type='primary'>Click</Button>
+        </Container>
+    )
+}
+
+export default Navbar
